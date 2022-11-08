af8fc595dedd777a6ee0a457534e48fbd7c864b7
e-young <93626945+simeunyoung@users.noreply.github.com>
2022-11-08 22:55:24 +0900

    Add files via upload

---
+++ layout/reset.ts
@@ -0,0 +1,95 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: "pretendard";
+    src: url("./font/Pretendard-SemiBold.woff");
+  }
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    font-family: 'pretendard';
+    -webkit-tap-highlight-color: transparent; 
+    outline: none; 
+    -ms-touch-action: manipulation; 
+    touch-action: manipulation; 
+    box-sizing: border-box;
+    letter-spacing: -0.7px;
+  }
+  *:focus { 
+    -webkit-tap-highlight-color: transparent; 
+    outline: none; 
+    -ms-touch-action: manipulation; 
+    touch-action: manipulation; 
+  }
+  body {
+    overflow: auto;
+    user-select: none;
+    /* overscroll-behavior: none; */
+    -webkit-overflow-scrolling: none;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+    
+  }
+  ol, ul, li {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  button:focus {
+    outline: none !important;
+  }
+  /* svg * {
+    transition: 0.08s;
+  } */
+  input {
+    border: none;
+    ::-webkit-outer-spin-button,
+    ::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    :disabled {
+      background-color: #f2f2f2;
+      color: #777;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+export default GlobalStyle;
